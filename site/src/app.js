7f33c617f3924679fbf7542da831a146565f3ac8
FernandoHazel <A01229544@itesm.mx>
2022-01-13 15:42:37 -0600

    arreglado el 404

--- site/src/app.js
+++ site/src/app.js
@@ -9,15 +9,18 @@ const rutaMain =require('./routes/main.js')
 app.use("/", express.static(__dirname+'/../public'))
 
 //Configurar ejs como el template engine de la app
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+
 
 
 // index page
 app.use('/', rutaMain)
-/*app.get('/', function(req, res) {
-    res.render('index');
-});*/
+
+//404
+app.use((req, res, next) => {
+    res.status(404).render('404')
+})
 
 app.listen(port);
 console.log(port + ' is the magic port');
