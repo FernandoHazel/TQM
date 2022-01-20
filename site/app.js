a33d72b11087ae4c2ae8066290d7cabf187e0581
FernandoHazel <A01229544@itesm.mx>
2022-01-19 21:24:00 -0600

    poco avance

---
+++ site/app.js
@@ -0,0 +1,37 @@
+//cargando los paquetes que necesitamos
+const express = require('express');
+const path = require("path")
+const rutaMain =require('./src/routes/main.js')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const config = require('./config/config')
+
+const app = express();
+
+//creando una dirección estática
+app.use("/", express.static(__dirname+'/public'))
+
+//morgan nos permite debuguear
+app.use(morgan('dev'))
+
+//Body parser hace que el servidor entienda peticiones POST
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+//Configurar ejs como el template engine de la app
+app.set('views', path.join(__dirname, '/'));
+app.set('view engine', 'ejs')
+
+//Seteamos un puerto para el momento de subir a producción
+app.set('port', process.env.PORT || 80)
+
+// index page
+app.use('/', rutaMain)
+
+//404
+app.use((req, res, next) => {
+    res.status(404).render('404')
+})
+
+app.listen(app.get('port'));
+console.log(`${app.get('port')} is the magic port`); 
